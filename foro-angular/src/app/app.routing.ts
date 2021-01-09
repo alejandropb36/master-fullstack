de168dead9d085cab3d8ed557ee2ea0c734c9bf9
Luis Alejandro Ponce Brizuela <alexponce36@hotmail.com>
2021-01-09 12:18:14 -0600

    foro-angular: crear pagina de ajustes

--- foro-angular/src/app/app.routing.ts
+++ foro-angular/src/app/app.routing.ts
@@ -6,6 +6,7 @@ import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from 'src/app/components/login/login.component';
 import { RegisterComponent } from 'src/app/components/register/register.component';
 import { HomeComponent } from './components/home/home.component';
+import { UserEditComponent } from './components/user-edit/user-edit.component';
 
 // Crear un array de rutas
 const appRoutes: Routes = [
@@ -13,6 +14,7 @@ const appRoutes: Routes = [
     { path: 'inicio', component: HomeComponent },
     { path: 'login', component: LoginComponent },
     { path: 'registro', component: RegisterComponent },
+    { path: 'ajustes', component: UserEditComponent },
     { path: '**', component: LoginComponent }
 ];
 
