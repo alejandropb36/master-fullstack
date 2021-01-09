4f26295d6e6fd3f4f389b8c76166a2a292ac880f
Luis Alejandro Ponce Brizuela <alexponce36@hotmail.com>
2021-01-09 00:20:17 -0600

    foro-angular: cerrar sesion

--- foro-angular/src/app/app.component.ts
+++ foro-angular/src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component , OnInit} from '@angular/core';
+import { Component , OnInit, DoCheck} from '@angular/core';
+import { Router, ActivatedRoute, Params } from '@angular/router';
 import { UserService } from './services/user.service';
 
 @Component({
@@ -7,13 +8,15 @@ import { UserService } from './services/user.service';
   styleUrls: ['./app.component.css'],
   providers: [ UserService ]
 })
-export class AppComponent implements OnInit {
+export class AppComponent implements OnInit, DoCheck {
   public title = 'Foro en angular';
   public identity: any;
   public token: string;
 
   constructor(
-    private _userService: UserService
+    private _userService: UserService,
+    private _router: Router,
+    private _route: ActivatedRoute
   ) {
     this.token = this._userService.getToken();
     this.identity = this._userService.getIdentity();
@@ -22,6 +25,18 @@ export class AppComponent implements OnInit {
   ngOnInit(): void {
     console.log(this.identity);
     console.log(this.token);
+  }
+
+  ngDoCheck(): void {
+    this.token = this._userService.getToken();
+    this.identity = this._userService.getIdentity();
+  }
+
+  logout(): void {
+    localStorage.clear();
+    this.identity = null;
+    this.token = '';
+    this._router.navigate(['/inicio']);
   }
 
 }
