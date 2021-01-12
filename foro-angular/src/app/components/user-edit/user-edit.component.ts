24108d9ca79115487e73255794248c55d77143d4
Luis Alejandro Ponce Brizuela <alexponce36@hotmail.com>
2021-01-12 17:21:35 -0600

    foro-angular: formulario para modificar usuario

--- foro-angular/src/app/components/user-edit/user-edit.component.ts
+++ foro-angular/src/app/components/user-edit/user-edit.component.ts
@@ -1,13 +1,35 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { UserService } from 'src/app/services/user.service';
+import { User } from 'src/app/models/user';
+import { Router, ActivatedRoute, Params } from '@angular/router';
 
 @Component({
   selector: 'app-user-edit',
   templateUrl: './user-edit.component.html',
-  styleUrls: ['./user-edit.component.css']
+  styleUrls: ['./user-edit.component.css'],
+  providers: [UserService]
 })
 export class UserEditComponent implements OnInit {
 
-  constructor() { }
+  public page_title: string;
+  public user: User;
+  public status: string;
+  public identity: any;
+  public token: string;
+
+  constructor(
+    private router: Router,
+    private route: ActivatedRoute,
+    private userService: UserService
+  ) {
+    this.page_title = 'Ajustes de usuario';
+    this.user = new User('', '', '', '', '', '', 'ROLE_USER', false);
+    this.status = '';
+    this.identity = this.userService.getIdentity();
+    this.token = this.userService.getToken();
+    this.user = this.identity;
+  }
 
   ngOnInit(): void {
   }
