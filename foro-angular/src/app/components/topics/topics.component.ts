044605b715fd6efa6505a91fb6fb05eedf5b6871
Luis Alejandro Ponce Brizuela <alexponce36@hotmail.com>
2021-02-08 17:54:02 -0600

    foro-angular: preparando componente de temas

--- foro-angular/src/app/components/topics/topics.component.ts
+++ foro-angular/src/app/components/topics/topics.component.ts
@@ -1,15 +1,36 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+
+import { Topic } from 'src/app/models/topic';
+import { TopicService } from 'src/app/services/topic.service';
 
 @Component({
   selector: 'app-topics',
   templateUrl: './topics.component.html',
-  styleUrls: ['./topics.component.css']
+  styleUrls: ['./topics.component.css'],
+  providers: [ TopicService ]
 })
 export class TopicsComponent implements OnInit {
+  public page_title: string;
+  public topics: Topic[];
+  public totalPages: number;
+  public page: number;
+  public next_page: number;
+  public prev_page: number;
 
-  constructor() { }
+
+  constructor(
+    private _route: ActivatedRoute,
+    private _router: Router,
+    private _topicService: TopicService
+  ) {
+    this.page_title = 'Temas';
+    this.topics = [];
+    this.totalPages = this.page = this.next_page = this.prev_page = 0;
+  }
 
   ngOnInit(): void {
+
   }
 
 }
