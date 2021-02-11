5b88df7188a806f1da430f2bc78a3940aafab9cf
Luis Alejandro Ponce Brizuela <alexponce36@hotmail.com>
2021-02-10 19:07:45 -0600

    foro-angular: sacar los datos del tema

--- foro-angular/src/app/components/topic-detail/topic-detail.component.ts
+++ foro-angular/src/app/components/topic-detail/topic-detail.component.ts
@@ -1,15 +1,48 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+
+import { Topic } from 'src/app/models/topic';
+import { TopicService } from 'src/app/services/topic.service';
 
 @Component({
   selector: 'app-topic-detail',
   templateUrl: './topic-detail.component.html',
-  styleUrls: ['./topic-detail.component.css']
+  styleUrls: ['./topic-detail.component.css'],
+  providers: [ TopicService ]
 })
 export class TopicDetailComponent implements OnInit {
+  public topic: Topic;
 
-  constructor() { }
+
+  constructor(
+    private _route: ActivatedRoute,
+    private _router: Router,
+    private _topicService: TopicService
+  ) {
+    this.topic = new Topic('', '', '', '', '', '', '', null);
+  }
 
   ngOnInit(): void {
+    this._route.params.subscribe(params => {
+      const id = params.id;
+      this.getTopic(id);
+    });
+  }
+
+  getTopic(id: string): void {
+    this._topicService.getTopic(id).subscribe(
+      response => {
+        if (response.topic && response.status === 'success') {
+          this.topic = response.topic;
+        } else {
+          this._router.navigate(['/inicio']);
+        }
+      },
+      error => {
+        console.log(error);
+        this._router.navigate(['/inicio']);
+      }
+    );
   }
 
 }
