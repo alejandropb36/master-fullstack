989fb9a39aae089e9f8511f47e6c49db5e807df7
Luis Alejandro Ponce Brizuela <alexponce36@hotmail.com>
2021-02-12 22:51:48 -0600

    foro-angular: formalrio de comentarios

--- foro-angular/src/app/app.component.ts
+++ foro-angular/src/app/app.component.ts
@@ -26,8 +26,6 @@ export class AppComponent implements OnInit, DoCheck {
   }
 
   ngOnInit(): void {
-    console.log(this.identity);
-    console.log(this.token);
   }
 
   ngDoCheck(): void {
--- foro-angular/src/app/components/topic-detail/topic-detail.component.ts
+++ foro-angular/src/app/components/topic-detail/topic-detail.component.ts
@@ -2,24 +2,34 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 
 import { Topic } from 'src/app/models/topic';
+import { Comment } from 'src/app/models/comment';
 import { TopicService } from 'src/app/services/topic.service';
+import { UserService } from 'src/app/services/user.service';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-topic-detail',
   templateUrl: './topic-detail.component.html',
   styleUrls: ['./topic-detail.component.css'],
-  providers: [ TopicService ]
+  providers: [ TopicService, UserService ]
 })
 export class TopicDetailComponent implements OnInit {
   public topic: Topic;
+  public comment: Comment;
+  public identity: any;
+  public token: string;
 
 
   constructor(
     private _route: ActivatedRoute,
     private _router: Router,
-    private _topicService: TopicService
+    private _topicService: TopicService,
+    private userService: UserService
   ) {
     this.topic = new Topic('', '', '', '', '', '', '', null);
+    this.identity = this.userService.getIdentity();
+    this.token = this.userService.getToken();
+    this.comment = new Comment('', '', '', this.identity._id);
   }
 
   ngOnInit(): void {
@@ -43,6 +53,10 @@ export class TopicDetailComponent implements OnInit {
         this._router.navigate(['/inicio']);
       }
     );
+  }
+
+  onSubmit(form: NgForm): void {
+    console.log(this.comment);
   }
 
 }
