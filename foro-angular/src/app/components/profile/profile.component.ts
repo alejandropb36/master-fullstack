b9d0431a22ab7736143a4b55b9d5dc03d8b9abfd
Luis Alejandro Ponce Brizuela <alexponce36@hotmail.com>
2021-02-17 22:18:29 -0600

    foro-angular: preparando componente del perfil

--- foro-angular/src/app/components/profile/profile.component.ts
+++ foro-angular/src/app/components/profile/profile.component.ts
@@ -1,15 +1,67 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from 'src/app/models/user';
+import { Topic } from 'src/app/models/topic';
+import { global } from 'src/app/services/global';
+import { TopicService } from 'src/app/services/topic.service';
+import { UserService } from 'src/app/services/user.service';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
-  styleUrls: ['./profile.component.css']
+  styleUrls: ['./profile.component.css'],
+  providers: [TopicService, UserService]
 })
 export class ProfileComponent implements OnInit {
 
-  constructor() { }
+  public user: User;
+  public url: string;
+  public topics: Topic[];
+
+  constructor(
+    private topicService: TopicService,
+    private userService: UserService,
+    private route: ActivatedRoute,
+    private router: Router
+  ) {
+    this.user = new User('', '', '', '', '', '', 'ROLE_USER', false);
+    this.url = global.url;
+    this.topics = [];
+  }
 
   ngOnInit(): void {
+
+    this.route.params.subscribe(params => {
+      const userId = params.id;
+      this.getUser(userId);
+      this.getTopics(userId);
+    });
+  }
+
+  getUser(userId: string): void {
+    this.userService.getUser(userId).subscribe(
+      response => {
+        if (response.status === 'success' && response.user) {
+          this.user = response.user;
+        }
+      },
+      error => {
+        console.log(error);
+      }
+    );
+  }
+
+  getTopics(userId: string): void {
+    this.topicService.getTopicsByUser(userId).subscribe(
+      response => {
+        if (response.status === 'success' && response. topics) {
+          this.topics = response.topics;
+        }
+      },
+      error => {
+        console.log(error);
+      }
+    );
   }
 
 }
