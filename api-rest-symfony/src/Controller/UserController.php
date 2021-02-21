5357fd8fdc62d21890194a71097fc33e48f9dcfc
Luis Alejandro Ponce Brizuela <alexponce36@hotmail.com>
2021-02-20 23:10:55 -0600

    api-symfony: configurando respuesta json

--- api-rest-symfony/src/Controller/UserController.php
+++ api-rest-symfony/src/Controller/UserController.php
@@ -4,33 +4,48 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 use App\Entity\User;
 use App\Entity\Video;
 
 class UserController extends AbstractController
 {
+
+    private function restjson($data) {
+        $json = $this->get('serializer')->serialize($data, 'json');
+
+        $response = new Response();
+
+        $response->setContent($json);
+
+        $response->headers->set('Content-Type', 'application/json');
+
+        return $response;
+    }
+
     public function index(): Response
     {
         $user_repo = $this->getDoctrine()->getRepository(User::class);
         $video_repo = $this->getDoctrine()->getRepository(Video::class);
 
         $users = $user_repo->findAll();
-        // $videos = $video_repo->findAll();
+        $user = $user_repo->find(1);
 
-        foreach($users as $user) {
-            echo "<h1>" . $user->getName() . "</h1>";
-
-            foreach($user->getVideos() as $video) {
-                echo "<h3>" . $video->getTitle() . " - " . $video->getUser()->getEmail() . "</h3>";
-            }
-        }
-
-        die();
-
-        return $this->json([
-            'users' => [],
+        $data = [
             'message' => 'Welcome to your new controller!',
             'path' => 'src/Controller/UserController.php',
-        ]);
+        ];
+        $videos = $video_repo->findAll();
+
+        // foreach($users as $user) {
+        //     echo "<h1>" . $user->getName() . "</h1>";
+
+        //     foreach($user->getVideos() as $video) {
+        //         echo "<h3>" . $video->getTitle() . " - " . $video->getUser()->getEmail() . "</h3>";
+        //     }
+        // }
+
+
+        return $this->restjson($videos);
     }
 }
