6a950e307819d57cc045c0b0d3da70a4b1834769
Luis Alejandro Ponce Brizuela <alexponce36@hotmail.com>
2021-04-01 18:10:39 -0600

    videos-angular: registro de usuarios

--- videos-angular/src/app/components/register/register.component.ts
+++ videos-angular/src/app/components/register/register.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { User } from 'src/app/models/user';
+import { UserService } from 'src/app/services/user.service';
 
 @Component({
   selector: 'app-register',
@@ -11,17 +12,31 @@ export class RegisterComponent implements OnInit {
 
   public pageTitle: string;
   public user: User;
+  public status: string;
 
-  constructor() {
+  constructor(private userService: UserService) {
     this.pageTitle = 'Register';
     this.user = new User(null, '', '', '', '', 'ROLE_USER', null);
+    this.status = '';
   }
 
   ngOnInit(): void {
   }
 
   onSubmit(form: NgForm): void {
-    console.log(this.user);
+    this.userService.register(this.user).subscribe(
+      response => {
+        console.log(response);
+        this.status = response.status;
+        if (this.status === 'success') {
+          form.reset();
+        }
+      },
+      error => {
+        this.status = 'error';
+        console.log(error);
+      }
+    );
   }
 
 }
