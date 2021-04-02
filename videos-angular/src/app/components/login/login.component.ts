2348785e47409d0f991dccf18a330ca593b629ab
Luis Alejandro Ponce Brizuela <alexponce36@hotmail.com>
2021-04-02 10:39:32 -0600

    videos-angular: formulario de login

--- videos-angular/src/app/components/login/login.component.ts
+++ videos-angular/src/app/components/login/login.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { User } from 'src/app/models/user';
+import { UserService } from 'src/app/services/user.service';
+
 
 @Component({
   selector: 'app-login',
@@ -8,12 +12,20 @@ import { Component, OnInit } from '@angular/core';
 export class LoginComponent implements OnInit {
 
   public pageTitle: string;
+  public user: User;
+  public status: string;
 
-  constructor() {
+  constructor(private userService: UserService) {
     this.pageTitle = 'Login';
+    this.user = new User(null, '', '', '', '', 'ROLE_USER', null);
+    this.status = '';
   }
 
   ngOnInit(): void {
+  }
+
+  onSubmit(form: NgForm): void {
+    console.log(this.user);
   }
 
 }
