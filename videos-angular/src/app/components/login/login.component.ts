d22ab1ef5e984ef43b11bc831b304564cd486f77
Luis Alejandro Ponce Brizuela <alexponce36@hotmail.com>
2021-04-02 11:08:46 -0600

    videos-angular: persistir datos del login

--- videos-angular/src/app/components/login/login.component.ts
+++ videos-angular/src/app/components/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute, Params } from '@angular/router';
 import { NgForm } from '@angular/forms';
 import { User } from 'src/app/models/user';
 import { UserService } from 'src/app/services/user.service';
@@ -17,7 +18,11 @@ export class LoginComponent implements OnInit {
   public status: string;
   public token: string;
 
-  constructor(private userService: UserService) {
+  constructor(
+    private userService: UserService,
+    private router: Router,
+    private route: ActivatedRoute,
+  ) {
     this.pageTitle = 'Login';
     this.user = new User(null, '', '', '', '', 'ROLE_USER', null, false);
     this.status = '';
@@ -42,6 +47,9 @@ export class LoginComponent implements OnInit {
                 this.token = response;
                 console.log(this.identity);
                 console.log(this.token);
+                localStorage.setItem('token', this.token);
+                localStorage.setItem('identity', JSON.stringify(this.identity));
+                this.router.navigate(['/inicio']);
               } else {
                 this.status = response.status;
               }
