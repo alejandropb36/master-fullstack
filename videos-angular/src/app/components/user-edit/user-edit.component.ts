aca6325ec9e8e8a580cda6b3aa492f24f7ed2948
Luis Alejandro Ponce Brizuela <alexponce36@hotmail.com>
2021-04-08 18:12:50 -0500

    videos-angular: formulario de ajustes de usuario

--- videos-angular/src/app/components/user-edit/user-edit.component.ts
+++ videos-angular/src/app/components/user-edit/user-edit.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { User } from 'src/app/models/user';
+import { UserService } from 'src/app/services/user.service';
 
 @Component({
   selector: 'app-user-edit',
@@ -7,13 +10,42 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserEditComponent implements OnInit {
 
-  pageTitle: string;
+  public pageTitle: string;
+  public user: User;
+  public status: string;
+  public identity: any;
+  public token: string;
 
-  constructor() {
-    this.pageTitle = 'Editar usuario'
+  constructor(private userService: UserService) {
+    this.pageTitle = 'Editar usuario';
+    this.status = '';
+    this.identity  = this.userService.getIdentity();
+    this.token = this.userService.getToken();
+    this.user = new User(
+      this.identity.sub,
+      this.identity.name,
+      this.identity.surname,
+      this.identity.email, '', 'ROLE_USER', null, false);
   }
 
   ngOnInit(): void {
+  }
+
+  onSubmit(form: NgForm): void {
+    console.log(this.user);
+    // this.userService.register(this.user).subscribe(
+    //   response => {
+    //     console.log(response);
+    //     this.status = response.status;
+    //     if (this.status === 'success') {
+    //       form.reset();
+    //     }
+    //   },
+    //   error => {
+    //     this.status = 'error';
+    //     console.log(error);
+    //   }
+    // );
   }
 
 }
