295695c16454d56735ea2ac03f64807c5d4c521f
Luis Alejandro Ponce Brizuela <alexponce36@hotmail.com>
2021-04-13 18:52:29 -0500

    videos-angular: componente preparado para crear videos

--- videos-angular/src/app/components/video-new/video-new.component.ts
+++ videos-angular/src/app/components/video-new/video-new.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router, Params } from '@angular/router';
+import { Video } from 'src/app/models/video';
+import { UserService } from 'src/app/services/user.service';
 
 @Component({
   selector: 'app-video-new',
@@ -7,10 +10,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class VideoNewComponent implements OnInit {
 
-  pageTitle: string;
+  public pageTitle: string;
+  public video: Video;
+  public identity: any;
+  public token: string;
 
-  constructor() {
+  constructor(
+    private router: Router,
+    private route: ActivatedRoute,
+    private userService: UserService
+  ) {
     this.pageTitle = 'Guardar un nuevo video'
+    this.identity = this.userService.getIdentity();
+    this.token = this.userService.getToken();
+    this.video = new Video(null, this.identity.sub, '', '', '', '', null, null);
   }
 
   ngOnInit(): void {
