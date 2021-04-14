7ca4d8b1da127fd009c8f8de858680c2c0f2ffe4
Luis Alejandro Ponce Brizuela <alexponce36@hotmail.com>
2021-04-13 23:03:22 -0500

    videos-angular: formulario para guardar video

--- videos-angular/src/app/components/video-new/video-new.component.ts
+++ videos-angular/src/app/components/video-new/video-new.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 import { ActivatedRoute, Router, Params } from '@angular/router';
 import { Video } from 'src/app/models/video';
 import { UserService } from 'src/app/services/user.service';
@@ -14,6 +15,7 @@ export class VideoNewComponent implements OnInit {
   public video: Video;
   public identity: any;
   public token: string;
+  public status: string;
 
   constructor(
     private router: Router,
@@ -24,9 +26,13 @@ export class VideoNewComponent implements OnInit {
     this.identity = this.userService.getIdentity();
     this.token = this.userService.getToken();
     this.video = new Video(null, this.identity.sub, '', '', '', '', null, null);
+    this.status = '';
   }
 
   ngOnInit(): void {
   }
-
+  
+  onSubmit(form: NgForm): void {
+    console.log(this.video);
+  }
 }
