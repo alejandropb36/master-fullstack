ba2ec5eeed92a2c6a062347ba739d337ac16a556
Luis Alejandro Ponce Brizuela <alexponce36@hotmail.com>
2021-04-13 23:15:04 -0500

    videos-angular: guardar video en la db

--- videos-angular/src/app/components/video-new/video-new.component.ts
+++ videos-angular/src/app/components/video-new/video-new.component.ts
@@ -3,6 +3,7 @@ import { NgForm } from '@angular/forms';
 import { ActivatedRoute, Router, Params } from '@angular/router';
 import { Video } from 'src/app/models/video';
 import { UserService } from 'src/app/services/user.service';
+import { VideoService } from 'src/app/services/video.service';
 
 @Component({
   selector: 'app-video-new',
@@ -20,7 +21,8 @@ export class VideoNewComponent implements OnInit {
   constructor(
     private router: Router,
     private route: ActivatedRoute,
-    private userService: UserService
+    private userService: UserService,
+    private videosService: VideoService
   ) {
     this.pageTitle = 'Guardar un nuevo video'
     this.identity = this.userService.getIdentity();
@@ -34,5 +36,18 @@ export class VideoNewComponent implements OnInit {
   
   onSubmit(form: NgForm): void {
     console.log(this.video);
+    this.videosService.create(this.video, this.token).subscribe(
+      response => {
+        this.status = response.status;
+        if (this.status === 'success') {
+          form.reset();
+          this.router.navigate(['/inicio']);
+        }
+      },
+      error => {
+        console.log(error);
+        this.status = 'error';
+      }
+    )
   }
 }
